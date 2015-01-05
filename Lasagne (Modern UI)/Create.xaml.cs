ebb9a427e40a0ac8447c93f66ddbe824a70f51ef
chrome289 <sidat289@gmail.com>
2015-01-05 21:19:18 +0530

    Added create task page

--- Lasagne (Modern UI)/Create.xaml.cs
+++ Lasagne (Modern UI)/Create.xaml.cs
@@ -1,48 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ookii.Dialogs.Wpf;
+using System;
+using System.Data.SQLite;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Lasagne__Modern_UI_
 {
-    /// <summary>
-    /// Interaction logic for Create.xaml
-    /// </summary>
+    
     public partial class Create : UserControl
     {
+        public static bool is_2_way = false;
         public Create()
         {
             InitializeComponent();
-        }
+            tb1.Text = "";
+            tb2.Text = "";
+            SQLiteConnection dbConnection;
+            dbConnection = new SQLiteConnection("Data Source=Database.sqlite;Version=3;");
+            dbConnection.Open();
 
-        private void bt2_Click(object sender, RoutedEventArgs e)
-        {
-
+            string sql = "create table if not exists sync (name varchar(100),First_Folder varchar(500), Second_Folder varchar(500),is_two_way varchar(10))";
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            command.ExecuteNonQuery();
+            dbConnection.Close();
         }
 
         private void bt1_Click(object sender, RoutedEventArgs e)
         {
+            VistaFolderBrowserDialog vd = new VistaFolderBrowserDialog();
+            vd.RootFolder = System.Environment.SpecialFolder.Desktop;
+            vd.ShowDialog();
+            tb1.Text = vd.SelectedPath;
+        }
 
+        private void bt2_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog vd = new VistaFolderBrowserDialog();
+            vd.ShowDialog();
+            tb2.Text = vd.SelectedPath;
         }
 
         private void bt3_Click(object sender, RoutedEventArgs e)
         {
-
+            
+            SQLiteConnection dbConnection;
+            dbConnection = new SQLiteConnection("Data Source=Database.sqlite;Version=3;");
+            dbConnection.Open();
+            string sql = "insert into sync (Name,First_Folder,Second_Folder,is_two_way) values ('" + tb3.Text + "','" + tb1.Text + "','" + tb2.Text + "','" + is_2_way.ToString() + "')";
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            command.ExecuteNonQuery();
+            dbConnection.Close();
+            tb1.Text = "";
+            tb2.Text = "";
+            tb3.Text = "";
+            String sMessageBoxText = "Sync task saved";
+            string sCaption = "Folder Sync";
+            MessageBoxButton btnMessageBox = MessageBoxButton.OK;
+            MessageBoxImage icnMessageBox = MessageBoxImage.Information;
+            MessageBoxResult rsltMessageBox = MessageBox.Show(sMessageBoxText, sCaption, btnMessageBox, icnMessageBox);
         }
 
         private void checkBox1_Checked(object sender, RoutedEventArgs e)
         {
-
+            if (is_2_way == false)
+                is_2_way = true;
+            else
+                is_2_way = false;
         }
 
     }
