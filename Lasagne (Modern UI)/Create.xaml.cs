99ff9802c0afff2e45381ca67294cce760221360
chrome289 <sidat289@gmail.com>
2015-01-05 20:29:24 +0530

    Intitial Commit

---
+++ Lasagne (Modern UI)/Create.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lasagne__Modern_UI_
+{
+    /// <summary>
+    /// Interaction logic for Create.xaml
+    /// </summary>
+    public partial class Create : UserControl
+    {
+        public Create()
+        {
+            InitializeComponent();
+        }
+
+        private void bt2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bt3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
